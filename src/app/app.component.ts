109511007a51722d97a05aeaa0e40a1102c4478c
Augusto <augstarachrafael@gmail.com>
2020-02-29 16:15:02 -0300

    Finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { QrCodeHelper } from 'ngx-kjua';
+import * as jsPDF from 'jspdf';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'wifi-share';
+  ssid = '';
+  pass = '';
+  wifi = '';
+  qrCode;
+
+  @ViewChild('htmlQR', {static: false}) htmlQR: ElementRef;
+  pdf = new jsPDF('p', 'mm', [297, 210]);
+  constructor(private renderer: Renderer2) {
+  }
+
+  onChange() {
+    console.log(this.htmlQR);
+    this.wifi = QrCodeHelper.makeWifi(this.ssid, this.pass);
+  }
+  gerarPDF() {
+    this.pdf.addHTML(this.htmlQR.nativeElement, () => {
+      this.pdf.save('wifi-share.pdf');
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
+import { NgxKjuaModule } from 'ngx-kjua';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxKjuaModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
